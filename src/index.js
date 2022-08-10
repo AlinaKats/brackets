69ba830f5c50c71b258f0c833fbebe549bd82d56
Alina Kats <alina.emelianova91@gmail.com>
2022-08-10 20:36:49 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  
+  //создадим объект для будущих пар ключ(закрывающая скобка): значение (открывающая)
+  let bracketsPair = {};
+  bracketsConfig.forEach((item) => {
+      bracketsPair[item[1]] = item[0]; //создадим пару значений объекта
+  });
+  
+  //сосдадим стек
+  let stack = [];
+
+  //будем помещать открывающие скобки внутрь, и проверять закрывающие на пару
+  for (let i = 0; i < arr.length; i++) {
+      let topElement = stack[stack.length - 1];
+      let current = arr[i];
+      
+      if (stack.length && topElement === bracketsPair[current]) {
+          stack.pop();
+        } else {
+          stack.push(current);
+      }
+  }
+  return stack.length === 0;
 }
